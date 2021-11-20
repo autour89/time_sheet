85ea6d7c6929f33df5dd7d9add6766b770864e5c
Oleksandr Shastun <autour89@outlook.com>
2021-11-20 21:27:05 +0200

    fixed

--- lib/screens/Widgets/duration_tags.dart
+++ lib/screens/Widgets/duration_tags.dart
@@ -3,34 +3,38 @@ import 'package:time_sheet/models/TagModel.dart';
 
 class DurationTags extends StatefulWidget {
   final List<TagModel> _hours = [];
-  TagModel selected = TagModel(hour: 0);
+  final int? duration;
 
   /// When you select a new tag this callback will be invoked
-  Function(int) onChange;
+  final Function(int) onChange;
 
-  /// selected value of tag
-  int get value => selected.hour;
-
-  DurationTags({Key? key, int? duration, required this.onChange})
-      : super(key: key) {
-    if (duration != null) selected = TagModel(hour: duration);
-  }
+  DurationTags({Key? key, this.duration, required this.onChange})
+      : super(key: key);
 
   @override
   State<StatefulWidget> createState() => _DurationTagsState();
 }
 
 class _DurationTagsState extends State<DurationTags> {
+  TagModel selected = TagModel(hour: 0);
+
+  /// selected value of tag
+  int get value => selected.hour;
+
+  _DurationTagsState() {}
+
   @override
   void initState() {
     for (var i = 1; i < 13; i++) widget._hours.add(TagModel(hour: i));
     super.initState();
+
+    selected = TagModel(hour: widget.duration ?? 0);
   }
 
   void _onTap(TagModel tag) {
     setState(() {
-      widget.selected = tag;
-      widget.onChange(widget.value);
+      selected = tag;
+      widget.onChange(value);
     });
   }
 
@@ -41,9 +45,8 @@ class _DurationTagsState extends State<DurationTags> {
       spacing: 10.0,
       children: [
         ...widget._hours.map((tag) => Chip(
-              backgroundColor: tag.value == widget.selected.value
-                  ? Colors.blue
-                  : Colors.grey[10],
+              backgroundColor:
+                  tag.value == selected.value ? Colors.blue : Colors.grey[10],
               label: InkWell(
                   onTap: () => _onTap(tag), child: Text('${tag.value}h')),
             ))
